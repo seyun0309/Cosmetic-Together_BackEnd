19627689acd44131e0da420f9d3461eaa398c753
seyun kim <>
2024-09-16 23:07:14 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/Capston/CosmeticTogether/CosmeticTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package Capston.CosmeticTogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmeticTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
