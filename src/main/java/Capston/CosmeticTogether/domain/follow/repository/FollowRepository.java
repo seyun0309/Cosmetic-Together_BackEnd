5ae191c8825f6084ea8a4c8c5993295fe463602f
seyun kim <>
2024-09-21 12:19:05 +0900

    feat : 사용자의 팔로워 가져오기, 사용자의 팔로잉 가져오기 쿼리 추가

---
+++ src/main/java/Capston/CosmeticTogether/domain/follow/repository/FollowRepository.java
@@ -0,0 +1,12 @@
+package Capston.CosmeticTogether.domain.follow.repository;
+
+import Capston.CosmeticTogether.domain.follow.domain.Follow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+    Optional<Follow> findByFollowerIdAndFollowingId(Long followingId, Long id);
+}
