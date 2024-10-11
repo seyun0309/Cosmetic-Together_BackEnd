bf95b54b948c0d2e0955d25044ade9ef977b4445
seyun kim <>
2024-10-11 22:04:41 +0900

    FEAT : @Enumerated(EnumType.STRING) 추가 및 팔로우/팔로잉 리스트 필드 추가 및 수정과 관련된 메서드 추가

--- src/main/java/Capston/CosmeticTogether/domain/member/domain/Member.java
+++ src/main/java/Capston/CosmeticTogether/domain/member/domain/Member.java
@@ -1,14 +1,16 @@
 package Capston.CosmeticTogether.domain.member.domain;
 
+import Capston.CosmeticTogether.domain.follow.domain.Follow;
+import Capston.CosmeticTogether.domain.member.dto.MemberProfileDTO;
 import Capston.CosmeticTogether.global.common.BaseEntity;
 import Capston.CosmeticTogether.global.enums.AuthType;
 import Capston.CosmeticTogether.global.enums.Role;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
 
 @SuperBuilder
 @Getter
@@ -47,11 +49,32 @@ public class Member extends BaseEntity {
     private String background_url;
 
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Role role;
 
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private AuthType authType;
+
+    @OneToMany(mappedBy = "following")
+    @JsonIgnore
+    private List<Follow> followerList;
+
+    @OneToMany(mappedBy = "follower")
+    @JsonIgnore
+    private List<Follow> followingList;
 
     @Setter
     private String refreshToken;
+
+    public void updateMemberInfo(MemberProfileDTO memberProfileDTO, Role role) {
+        this.userName = memberProfileDTO.getUserName();
+        this.email = memberProfileDTO.getEmail();
+        this.phone = memberProfileDTO.getPhone();
+        this.nickname = memberProfileDTO.getNickname();
+        this.address = memberProfileDTO.getAddress();
+        this.profile_url = memberProfileDTO.getProfile_url();
+        this.background_url = memberProfileDTO.getBackground_url();
+        this.role = role;
+    }
 }
