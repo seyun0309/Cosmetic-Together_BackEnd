58c4402443d36b931a35641f7a4462e948ae0bff
seyun kim <>
2024-10-15 10:30:51 +0900

    FEAT : 게시글 수정 정보 화면에 띄우는 API 추가 및 Swagger 문서 수정

--- src/main/java/Capston/CosmeticTogether/domain/board/controller/BoardController.java
+++ src/main/java/Capston/CosmeticTogether/domain/board/controller/BoardController.java
@@ -1,7 +1,8 @@
 package Capston.CosmeticTogether.domain.board.controller;
 
 import Capston.CosmeticTogether.domain.board.dto.request.CreateBoardRequestDTO;
-import Capston.CosmeticTogether.domain.board.dto.response.BoardResponseDTO;
+import Capston.CosmeticTogether.domain.board.dto.response.GetBoardResponseDTO;
+import Capston.CosmeticTogether.domain.board.dto.response.UpdateFormResponseDTO;
 import Capston.CosmeticTogether.domain.board.service.BoardService;
 import Capston.CosmeticTogether.domain.board.service.S3ImageService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,58 +27,61 @@ public class BoardController {
 
     // 게시글 등록
     @PostMapping()
-    @Operation(summary = "게시글 작성", description = "image(게시글 사진), request(게시글 내용)으로 서버에 데이터를 보내면 게시글 등록됩니다 / 토큰 필요")
-    public ResponseEntity<String> uploadBoard(@RequestPart(required = false, name = "image") MultipartFile image,
+    @Operation(summary = "[API] 게시글 작성 - 토큰 필요", description = "이미지 파일(images)과 게시글 내용(request)을 서버에 전송하여 게시글을 등록합니다 / 이미지는 선택사항, 게시글 내용은 필수")
+    public ResponseEntity<String> uploadBoard(@RequestPart(required = false, name = "images") List<MultipartFile> images,
                                               @RequestPart(name = "request") @Valid CreateBoardRequestDTO createBoardRequestDTO) {
-        String imgUrl = s3ImageService.upload(image);
-        boardService.uploadBoard(imgUrl, createBoardRequestDTO);
 
+        boardService.uploadBoard(images, createBoardRequestDTO);
         return ResponseEntity.ok("게시글이 등록되었습니다");
     }
 
     // 게시글 단일조회
     //TODO 댓글도 같이 리턴해야 함
-    //TODO 찜 개수 리턴
     @GetMapping("/{boardId}")
-    @Operation(summary = "게시글 단일 조회", description = "게시글 id를 서버에 보내주면 하나의 게시글을 터치했을 때 해당 게시글의 세부 내용(댓글 포함)을 리턴합니다")
-    public ResponseEntity<BoardResponseDTO> getBoard(@PathVariable("boardId") Long boardId) {
-        BoardResponseDTO response = boardService.getBoard(boardId);
+    @Operation(summary = "[API] 게시글 단일 조회", description = "해당 게시글의 boardId를 URL 경로에 포함시켜 하나의 게시글 정보를 조회합니다. "
+            + "결과로는 작성자 정보, 게시글의 내용(이미지 포함), 작성 일자 데이터를 포함한 JSON 형식의 응답이 반환됩니다.")
+    public ResponseEntity<GetBoardResponseDTO> getBoard(@PathVariable("boardId") Long boardId) {
+        GetBoardResponseDTO response = boardService.getBoard(boardId);
         return ResponseEntity.ok(response);
     }
 
     // 최신 게시글 조회
-    //TODO 찜 개수 리턴
     @GetMapping("/recent")
-    @Operation(summary = "최신 게시글 조회", description = "최신 게시글들(List)을 리턴합니다")
-    public ResponseEntity<List<BoardResponseDTO>> getRecentBoard() {
-        List<BoardResponseDTO> response = boardService.getRecentBoard();
+    @Operation(summary = "[API] 최신 게시글 조회", description = "최신 게시글들(List)을 리턴합니다")
+    public ResponseEntity<List<GetBoardResponseDTO>> getRecentBoard() {
+        List<GetBoardResponseDTO> response = boardService.getRecentBoard();
         return ResponseEntity.ok(response);
     }
 
     // 팔로잉 게시글 조회
-    //TODO 찜 개수 리턴
     @GetMapping("/following")
-    @Operation(summary = "팔로잉 게시글 조회", description = "사용자가 팔로잉한 사용자의 최신 게시글들(List)을 리턴합니다")
-    public ResponseEntity<List<BoardResponseDTO>> getFollowingMemberBoard() {
-        List<BoardResponseDTO> response = boardService.getFollowingMemberBoard();
+    @Operation(summary = "[API] 팔로잉 게시글 조회", description = "사용자가 팔로잉한 사용자의 최신 게시글들(List)을 리턴합니다")
+    public ResponseEntity<List<GetBoardResponseDTO>> getFollowingMemberBoard() {
+        List<GetBoardResponseDTO> response = boardService.getFollowingMemberBoard();
         return ResponseEntity.ok(response);
     }
 
+    // 게시글 수정 정보 띄우기
+    @PostMapping("/info/{boardId}")
+    @Operation(summary = "[UI] 게시글 수정 정보 화면에 띄우기", description = "해당 게시글의 boardId를 URL 경로에 포함시켜 수정 단계에서 화면에 해당 게시글 정보를 화면에 띄우는 역할")
+    public ResponseEntity<UpdateFormResponseDTO> getBoardUpdateInfo(@PathVariable("boardId") Long boardId) {
+        UpdateFormResponseDTO response = boardService.getBoardUpdateInfo(boardId);
+        return ResponseEntity.ok(response);
+    }
     // 게시글 수정
     @PatchMapping("/{boardId}")
-    @Operation(summary = "본인 게시글 수정", description = "url에 boardId를 넣고 해당 api를 호출하고 image(게시글 사진), request(게시글 내용)을 서버에 보내주면 게시글 수정이 완료됩니다 / 토큰 필요")
+    @Operation(summary = "[API] 본인 게시글 수정 - 토큰 필요", description = "해당 게시글의 boardId를 URL 경로에 포함하고 이미지 파일(images)과 게시글 내용(request)을 서버에 전송하여 게시글 수정")
     public ResponseEntity<String> updateBoard(@PathVariable("boardId") Long boardId,
-                                              @RequestPart(required = false, name = "image") MultipartFile image,
+                                              @RequestPart(required = false, name = "images") List<MultipartFile> images,
                                               @RequestPart(name = "request") @Valid CreateBoardRequestDTO createBoardRequestDTO) {
 
-        String imgUrl = s3ImageService.upload(image);
-        boardService.updateBoard(boardId, imgUrl, createBoardRequestDTO);
+        boardService.updateBoard(boardId, images, createBoardRequestDTO);
         return ResponseEntity.ok("게시글이 수정되었습니다");
     }
 
     // 게시글 삭제
     @DeleteMapping("/{boardId}")
-    @Operation(summary = "본인 게시글 삭제", description = "url에 boardId를 넣고 해당 api를 호출하면 게시글이 삭제됩니다  / 토큰 필요")
+    @Operation(summary = "[API] 본인 게시글 삭제 - 토큰 필요", description = "해당 게시글의 boardId를 URL 경로에 포함시켜 게시글 삭제")
     public ResponseEntity<String> deleteBoard(@PathVariable("boardId") Long boardId) {
         boardService.deleteBoard(boardId);
         return ResponseEntity.ok("게시글이 삭제되었습니다");
