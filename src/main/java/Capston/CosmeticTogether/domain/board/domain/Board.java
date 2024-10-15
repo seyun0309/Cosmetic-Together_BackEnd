34bc3caa918785eb782c0e4bf1e8bca20fa93f0d
seyun kim <>
2024-10-15 10:30:02 +0900

    FEAT : 게시글 앤티티 추가

--- src/main/java/Capston/CosmeticTogether/domain/board/domain/Board.java
+++ src/main/java/Capston/CosmeticTogether/domain/board/domain/Board.java
@@ -1,5 +1,7 @@
 package Capston.CosmeticTogether.domain.board.domain;
 
+import Capston.CosmeticTogether.domain.favorites.domain.Favorites;
+import Capston.CosmeticTogether.domain.form.domain.Form;
 import Capston.CosmeticTogether.domain.member.domain.Member;
 import Capston.CosmeticTogether.global.common.BaseEntity;
 import jakarta.persistence.*;
@@ -8,6 +10,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SuperBuilder
 @Getter
@@ -20,13 +25,14 @@ public class Board extends BaseEntity {
     @Column
     private String description;
 
-    @Column
-    private String board_url;
-
-    @Column(nullable = false)
-    private int favorite_count;
+    @OneToMany(mappedBy = "board")
+    private List<BoardImage> boardImages;
 
     @JoinColumn(nullable = false)
     @ManyToOne
     private Member member;
+
+    public void update(String description) {
+        this.description = description;
+    }
 }
