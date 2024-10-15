db77c2cc389ecf080ec284ada64327045d6b5f26
seyun kim <>
2024-10-15 10:48:15 +0900

    FEAT : 팔로워, 팔로잉 메서드 반환값 변경 및 swagger 수정

--- src/main/java/Capston/CosmeticTogether/domain/follow/controller/FollowController.java
+++ src/main/java/Capston/CosmeticTogether/domain/follow/controller/FollowController.java
@@ -1,5 +1,6 @@
 package Capston.CosmeticTogether.domain.follow.controller;
 
+import Capston.CosmeticTogether.domain.follow.dto.response.GetFollowAndFollowingMemberDTO;
 import Capston.CosmeticTogether.domain.follow.service.FollowService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -17,14 +18,14 @@ public class FollowController {
     private final FollowService followService;
 
     @PostMapping("/follow/{followingId}")
-    @Operation(summary = "상대방 팔로우 하기", description = "상대방의 id를 전달하면 팔로우가 됩니다  / 토큰 필요")
+    @Operation(summary = "[API] 상대방 팔로우 하기 - 토큰 필요", description = "상대방 id를 URL 경로에 포함시켜 전달하면 팔로우가 됩니다")
     public ResponseEntity<String> followMember(@PathVariable("followingId") Long followingId) {
         String followingMemberNickName = followService.followMember(followingId);
         return ResponseEntity.ok(followingMemberNickName + " 님을 팔로우하였습니다");
     }
 
     @PostMapping("/unfollow/{unfollowingId}")
-    @Operation(summary = "상대방 언팔로우 하기", description = "상대방의 id를 전달하면 언팔로우가 됩니다  / 토큰 필요")
+    @Operation(summary = "[API] 상대방 언팔로우 하기 - 토큰 필요", description = "상대방 id를 URL 경로에 포함시켜 언팔로우가 됩니다")
     public ResponseEntity<String> unfollowMember(@PathVariable("unfollowingId") Long unfollowingId) {
         String unfollowingMemberNickName = followService.unfollowMember(unfollowingId);
         return ResponseEntity.ok(unfollowingMemberNickName + " 님을 팔로우 취소하였습니다");
@@ -32,16 +33,16 @@ public class FollowController {
 
     //TODO 사진 같이 전달
     @GetMapping("/followers")
-    @Operation(summary = "본인의 팔로워 가져오기", description = "본인의 팔로워를 사진, 이름과 함께 전달합니다  / 토큰 필요")
-    public ResponseEntity<List<String>> getFollowers() {
-        List<String> followerMemberList = followService.getFollowers();
-        return ResponseEntity.ok(followerMemberList);
+    @Operation(summary = "[API] 본인의 팔로워 가져오기 - 토큰 필요", description = "본인의 팔로워를 사진, 이름과 함께 전달합니다")
+    public ResponseEntity<List<GetFollowAndFollowingMemberDTO>> getFollowers() {
+        List<GetFollowAndFollowingMemberDTO> response = followService.getFollowers();
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/followings")
-    @Operation(summary = "본인의 팔로잉 가져오기", description = "본인의 팔로잉을 사진, 이름과 함께 전달합니다  / 토큰 필요")
-    public ResponseEntity<List<String>> getFollowings() {
-        List<String> followingMemberList = followService.getFollowings();
-        return ResponseEntity.ok(followingMemberList);
+    @Operation(summary = "[API] 본인의 팔로잉 가져오기 - 토큰 필요", description = "본인의 팔로잉을 사진, 이름과 함께 전달합니다")
+    public ResponseEntity<List<GetFollowAndFollowingMemberDTO>> getFollowings() {
+        List<GetFollowAndFollowingMemberDTO> response = followService.getFollowings();
+        return ResponseEntity.ok(response);
     }
 }
