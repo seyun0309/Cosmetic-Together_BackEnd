a1a47865a344deede4a5fa9a9588230f7697be1b
seyun kim <>
2024-10-19 18:15:56 +0900

    FEAT : 테스트용 사용자 계정 추가

--- src/main/java/Capston/CosmeticTogether/global/DataLoader.java
+++ src/main/java/Capston/CosmeticTogether/global/DataLoader.java
@@ -1,5 +1,6 @@
 package Capston.CosmeticTogether.global;
 
+import Capston.CosmeticTogether.domain.board.repository.BoardRepository;
 import Capston.CosmeticTogether.domain.member.domain.Member;
 import Capston.CosmeticTogether.domain.member.repository.MemberRepository;
 import Capston.CosmeticTogether.global.enums.AuthType;
@@ -11,16 +12,21 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class DataLoader implements ApplicationRunner {
     private final MemberRepository memberRepository;
+    private final BoardRepository boardRepository;
     private final JdbcTemplate jdbcTemplate;
     private final PasswordEncoder passwordEncoder;
 
 
     @Autowired
-    public DataLoader(MemberRepository memberRepository, JdbcTemplate jdbcTemplate, PasswordEncoder passwordEncoder) {
+    public DataLoader(MemberRepository memberRepository, BoardRepository boardRepository, JdbcTemplate jdbcTemplate, PasswordEncoder passwordEncoder) {
         this.memberRepository = memberRepository;
+        this.boardRepository = boardRepository;
         this.jdbcTemplate = jdbcTemplate;
         this.passwordEncoder = passwordEncoder;
     }
@@ -28,24 +34,23 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        // 부서 초기 데이터 삽입
         memberRepository.deleteAll();
+        boardRepository.deleteAll();
 
         // 시퀀스 초기화
         jdbcTemplate.execute("ALTER SEQUENCE member_id_seq RESTART WITH 1");
+        jdbcTemplate.execute("ALTER SEQUENCE board_id_seq RESTART WITH 1");
 
         String encodedPassword = passwordEncoder.encode("1234");
 
-        Member member = Member.builder()
-                .userName("사용자A")
-                .email("asdf1234@naver.com")
-                .password(encodedPassword)
-                .phone("010-1111-2222")
-                .nickname("사용자A")
-                .address("서울특별시 !!!")
-                .role(Role.USER)
-                .authType(AuthType.REGULAR)
-                .build();
-        memberRepository.save(member);
+        List<Member> members = Arrays.asList(
+                new Member("사용자A", "test1@naver.com", encodedPassword, "010-1111-2222", "사용자A", "서울특별시", Role.USER, AuthType.REGULAR),
+                new Member("사용자B", "test2@naver.com", encodedPassword, "010-1111-2222", "사용자B", "서울특별시", Role.USER, AuthType.REGULAR),
+                new Member("사용자C", "test3@naver.com", encodedPassword, "010-1111-2222", "사용자C", "서울특별시", Role.USER, AuthType.REGULAR),
+                new Member("사용자D", "test4@naver.com", encodedPassword, "010-1111-2222", "사용자D", "서울특별시", Role.USER, AuthType.REGULAR),
+                new Member("사용자E", "test5@naver.com", encodedPassword, "010-1111-2222", "사용자E", "서울특별시", Role.USER, AuthType.REGULAR)
+        );
+
+        memberRepository.saveAll(members);
     }
 }
