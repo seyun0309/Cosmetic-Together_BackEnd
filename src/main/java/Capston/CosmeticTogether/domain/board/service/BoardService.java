099c991801fc767eabd4c6a155734956db99c13c
seyun kim <>
2024-11-03 19:18:19 +0900

    FEAT : 게시글 조회시 댓글 포함해서 리턴하는 것으로 변경

--- src/main/java/Capston/CosmeticTogether/domain/board/service/BoardService.java
+++ src/main/java/Capston/CosmeticTogether/domain/board/service/BoardService.java
@@ -3,10 +3,14 @@ package Capston.CosmeticTogether.domain.board.service;
 import Capston.CosmeticTogether.domain.board.domain.Board;
 import Capston.CosmeticTogether.domain.board.domain.BoardImage;
 import Capston.CosmeticTogether.domain.board.dto.request.CreateBoardRequestDTO;
-import Capston.CosmeticTogether.domain.board.dto.response.GetBoardResponseDTO;
+import Capston.CosmeticTogether.domain.board.dto.response.BoardSummaryResponseDTO;
+import Capston.CosmeticTogether.domain.board.dto.response.CommentResponseDTO;
+import Capston.CosmeticTogether.domain.board.dto.response.BoardDetailResponseDTO;
 import Capston.CosmeticTogether.domain.board.dto.response.UpdateBoardResponseDTO;
 import Capston.CosmeticTogether.domain.board.repository.BoardImageRepository;
 import Capston.CosmeticTogether.domain.board.repository.BoardRepository;
+import Capston.CosmeticTogether.domain.comment.domain.Comment;
+import Capston.CosmeticTogether.domain.comment.repository.CommentRepository;
 import Capston.CosmeticTogether.domain.follow.domain.Follow;
 import Capston.CosmeticTogether.domain.likes.repository.LikesRepository;
 import Capston.CosmeticTogether.domain.member.domain.Member;
@@ -36,6 +40,7 @@ public class BoardService {
     private final MemberService memberService;
     private final LikesRepository likesRepository;
     private final S3ImageService s3ImageService;
+    private final CommentRepository commentRepository;
 
     @Transactional
     public void uploadBoard(List<MultipartFile> images, CreateBoardRequestDTO createBoardRequestDTO) {
@@ -71,9 +76,11 @@ public class BoardService {
         board.setBoardImages(boardImages);
     }
 
-    public GetBoardResponseDTO getBoard(Long boardId) {
+    public BoardDetailResponseDTO getBoard(Long boardId) {
         // 1. boardId로 board 가져오기
         Board board = boardRepository.findDeleteAtIsNullById(boardId).orElseThrow(() -> new BusinessException("존재하는 게시글이 아닙니다", ErrorCode.BOARD_NOT_FOUND));
+        List<Comment> comments = commentRepository.findByBoard(board);
+
         long likeCount = likesRepository.countLikesByBoardId(board.getId());
 
         // 2. 작성자 추출
@@ -81,54 +88,63 @@ public class BoardService {
 
         // 3. BoardImage 리스트에서 이미지 URL 추출
         List<String> imageUrls = board.getBoardImages().stream()
-                .map(BoardImage::getImageUrl)
+                .map(BoardImage::getBoardUrl)
                 .collect(Collectors.toList());
 
         // 4. 작성시간 포맷팅
         String postTime = formatTime(board.getCreatedAt());
 
         // 5. 매핑해서 리턴
-        return GetBoardResponseDTO.builder()
+        List<CommentResponseDTO> commentResponseDTOList = new ArrayList<>();
+        for(Comment comment : comments) {
+            CommentResponseDTO commentResponseDTO = new CommentResponseDTO(comment.getCommenter().getNickname(), comment.getCommenter().getProfileUrl(), formatTime(comment.getCreatedAt()));
+            commentResponseDTOList.add(commentResponseDTO);
+        }
+
+        return BoardDetailResponseDTO.builder()
                 .writerNickName(writer.getNickname())
-                .profileUrl(board.getMember().getProfile_url())
+                .profileUrl(board.getMember().getProfileUrl())
                 .description(board.getDescription())
                 .boardUrl(imageUrls)
                 .likeCount(likeCount)
                 .postTime(postTime)
+                .comments(commentResponseDTOList)
                 .build();
     }
 
-    public List<GetBoardResponseDTO> getRecentBoard() {
+    public List<BoardSummaryResponseDTO> getRecentBoard() {
         // 1. 모든 게시글 불러오기
         List<Board> all = boardRepository.findDeleteAtIsNullAll();
-        List<GetBoardResponseDTO> response = new ArrayList<>();
+        List<BoardSummaryResponseDTO> response = new ArrayList<>();
 
         // 2. 매핑해서 리턴
         for(Board board : all) {
             long likeCount = likesRepository.countLikesByBoardId(board.getId());
+            long commentCount = commentRepository.countByBoard(board);
 
             // 2-1. BoardImage 리스트에서 이미지 URL 추출
             List<String> imageUrls = board.getBoardImages().stream()
-                    .map(BoardImage::getImageUrl)
+                    .map(BoardImage::getBoardUrl)
                     .collect(Collectors.toList());
 
             // 2-2. 작성시간 포맷팅
             String postTime = formatTime(board.getCreatedAt());
 
-            GetBoardResponseDTO getBoardResponseDTO = GetBoardResponseDTO.builder()
+            BoardSummaryResponseDTO boardSummaryResponseDTO = BoardSummaryResponseDTO.builder()
                     .writerNickName(board.getMember().getNickname())
-                    .profileUrl(board.getMember().getProfile_url())
+                    .profileUrl(board.getMember().getProfileUrl())
                     .description(board.getDescription())
                     .boardUrl(imageUrls)
                     .likeCount(likeCount)
                     .postTime(postTime)
+                    .commentCount(commentCount)
                     .build();
-            response.add(getBoardResponseDTO);
+            response.add(boardSummaryResponseDTO);
         }
         return response;
     }
 
-    public List<GetBoardResponseDTO> getFollowingMemberBoard() {
+    public List<BoardSummaryResponseDTO> getFollowingMemberBoard() {
         // 1. 로그인한 사용자 정보 가져오기
         Member loginMember = memberService.getMemberFromSecurityDTO((SecurityMemberDTO) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
 
@@ -142,58 +158,62 @@ public class BoardService {
         List<Board> followingBoards = boardRepository.findByFollowingMembers(followingMembers);
 
         // 4. 매핑해서 리턴
-        List<GetBoardResponseDTO> response = new ArrayList<>();
+        List<BoardSummaryResponseDTO> response = new ArrayList<>();
 
         for(Board board : followingBoards) {
             long likeCount = likesRepository.countLikesByBoardId(board.getId());
+            long commentCount = commentRepository.countByBoard(board);
 
             // 2-1. BoardImage 리스트에서 이미지 URL 추출
             List<String> imageUrls = board.getBoardImages().stream()
-                    .map(BoardImage::getImageUrl)
+                    .map(BoardImage::getBoardUrl)
                     .collect(Collectors.toList());
 
             // 2-2. 작성시간 포맷팅
             String postTime = formatTime(board.getCreatedAt());
 
-            GetBoardResponseDTO dto = GetBoardResponseDTO.builder()
+            BoardSummaryResponseDTO dto = BoardSummaryResponseDTO.builder()
                     .writerNickName(board.getMember().getNickname())
-                    .profileUrl(board.getMember().getProfile_url())
+                    .profileUrl(board.getMember().getProfileUrl())
                     .description(board.getDescription())
                     .boardUrl(imageUrls)
                     .likeCount(likeCount)
                     .postTime(postTime)
+                    .commentCount(commentCount)
                     .build();
             response.add(dto);
         }
         return response;
     }
 
-    public List<GetBoardResponseDTO> searchBoardByKeyword(String keyword) {
+    public List<BoardSummaryResponseDTO> searchBoardByKeyword(String keyword) {
         // 1. 키워드가 포함된 게시글 불러오기
         List<Board> boardList = boardRepository.findByDescriptionContaining(keyword);
-        List<GetBoardResponseDTO> response = new ArrayList<>();
+        List<BoardSummaryResponseDTO> response = new ArrayList<>();
 
         // 2. 매핑해서 리턴
         for(Board board : boardList) {
             long likeCount = likesRepository.countLikesByBoardId(board.getId());
+            long commentCount = commentRepository.countByBoard(board);
 
             // 2-1. BoardImage 리스트에서 이미지 URL 추출
             List<String> imageUrls = board.getBoardImages().stream()
-                    .map(BoardImage::getImageUrl)
+                    .map(BoardImage::getBoardUrl)
                     .collect(Collectors.toList());
 
             // 2-2. 작성시간 포맷팅
             String postTime = formatTime(board.getCreatedAt());
 
-            GetBoardResponseDTO getBoardResponseDTO = GetBoardResponseDTO.builder()
+            BoardSummaryResponseDTO boardSummaryResponseDTO = BoardSummaryResponseDTO.builder()
                     .writerNickName(board.getMember().getNickname())
-                    .profileUrl(board.getMember().getProfile_url())
+                    .profileUrl(board.getMember().getProfileUrl())
                     .description(board.getDescription())
                     .boardUrl(imageUrls)
                     .likeCount(likeCount)
                     .postTime(postTime)
+                    .commentCount(commentCount)
                     .build();
-            response.add(getBoardResponseDTO);
+            response.add(boardSummaryResponseDTO);
         }
         return response;
     }
@@ -211,7 +231,7 @@ public class BoardService {
 
         // 3. 정보 매핑해서 리턴
         List<String> imageUrls = board.getBoardImages().stream()
-                .map(BoardImage::getImageUrl)
+                .map(BoardImage::getBoardUrl)
                 .collect(Collectors.toList());
 
         return UpdateBoardResponseDTO.builder()
@@ -237,7 +257,7 @@ public class BoardService {
             if (images != null && !images.isEmpty()) {
                 for(BoardImage boardImage : board.getBoardImages()) {
                     boardImageRepository.delete(boardImage);
-                    s3ImageService.deleteImageFromS3(boardImage.getImageUrl());
+                    s3ImageService.deleteImageFromS3(boardImage.getBoardUrl());
                 }
                 saveBoardImages(images, board);
             }
