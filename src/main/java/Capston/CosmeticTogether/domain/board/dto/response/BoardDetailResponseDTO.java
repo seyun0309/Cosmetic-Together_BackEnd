4b850b703570b81442be43719809731a2898a98d
seyun kim <>
2024-11-03 19:17:42 +0900

    FEAT : 변수명 면경 및 댓글 필드 추가

--- src/main/java/Capston/CosmeticTogether/domain/board/dto/response/BoardDetailResponseDTO.java
+++ src/main/java/Capston/CosmeticTogether/domain/board/dto/response/BoardDetailResponseDTO.java
@@ -10,11 +10,12 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @Builder
-public class GetBoardResponseDTO {
+public class BoardDetailResponseDTO {
     private String writerNickName;
     private String profileUrl;
     private String description;
     private List<String> boardUrl;
     private Long likeCount;
     private String postTime;
+    private List<CommentResponseDTO> comments;
 }
