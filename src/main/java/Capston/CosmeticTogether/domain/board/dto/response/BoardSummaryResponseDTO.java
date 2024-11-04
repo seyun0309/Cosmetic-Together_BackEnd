b3a48570f2e6339aa8c9c8d738c22bc71b3143bd
seyun kim <>
2024-11-04 14:12:22 +0900

    FEAT : 게시글 리스트로 조회시 반환되는 DTO

---
+++ src/main/java/Capston/CosmeticTogether/domain/board/dto/response/BoardSummaryResponseDTO.java
@@ -0,0 +1,22 @@
+package Capston.CosmeticTogether.domain.board.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class BoardSummaryResponseDTO {
+
+    private String writerNickName;
+    private String profileUrl;
+    private String description;
+    private List<String> boardUrl;
+    private Long likeCount;
+    private String postTime;
+    private Long commentCount;
+}
