e3a7a2983aec7ec4a2516167e9a3624ddfdd4712
seyun kim <>
2024-11-13 09:55:38 +0900

    FEAT : 폼 생성후 리턴하는 DTO 추가

---
+++ src/main/java/Capston/CosmeticTogether/domain/form/dto/resonse/CreateFormResponseDTO.java
@@ -0,0 +1,15 @@
+package Capston.CosmeticTogether.domain.form.dto.resonse;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class CreateFormResponseDTO {
+    private Long formId;
+    private List<Long> productId;
+}
