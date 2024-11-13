fe24842e7f256e1d467d76fc4cda50ad33d4225b
seyun kim <>
2024-11-13 09:55:04 +0900

    FEAT : @NoArgsConstructor 추가

--- src/main/java/Capston/CosmeticTogether/global/auth/dto/response/SignUpResponseDTO.java
+++ src/main/java/Capston/CosmeticTogether/global/auth/dto/response/SignUpResponseDTO.java
@@ -3,9 +3,11 @@ package Capston.CosmeticTogether.global.auth.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class SignUpResponseDTO {
     private String userName;
