344c1cda981e3ab64910acb79f0ceb1b726147e1
seyun kim <>
2024-12-03 10:08:25 +0900

    FEAT : 롬복 임포트문 변경 및 isValid 필드 추가

--- src/main/java/Capston/CosmeticTogether/domain/follow/domain/Follow.java
+++ src/main/java/Capston/CosmeticTogether/domain/follow/domain/Follow.java
@@ -4,10 +4,7 @@ package Capston.CosmeticTogether.domain.follow.domain;
 import Capston.CosmeticTogether.domain.member.domain.Member;
 import Capston.CosmeticTogether.global.common.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
@@ -25,4 +22,8 @@ public class Follow extends BaseEntity {
     @JoinColumn(name = "following_member_id")
     @ManyToOne
     private Member following;
+
+    @Column(nullable = false)
+    @Setter
+    private boolean isValid;
 }
