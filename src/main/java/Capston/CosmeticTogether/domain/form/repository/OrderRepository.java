670fe2471ee6ccc506789947ccf842c33a76f024
seyun kim <>
2024-12-03 10:10:38 +0900

    FEAT : 사용자가 주문한 폼 조회 레파지토리 추가

--- src/main/java/Capston/CosmeticTogether/domain/form/repository/OrderRepository.java
+++ src/main/java/Capston/CosmeticTogether/domain/form/repository/OrderRepository.java
@@ -1,10 +1,14 @@
 package Capston.CosmeticTogether.domain.form.repository;
 
+import Capston.CosmeticTogether.domain.form.domain.Form;
 import Capston.CosmeticTogether.domain.form.domain.Order;
+import Capston.CosmeticTogether.domain.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
@@ -13,4 +17,10 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("SELECT CASE WHEN COUNT(o) > 0 THEN true ELSE false END FROM Order o JOIN o.orderProducts op WHERE op.product.id IN (SELECT p.id FROM Form f JOIN f.product p WHERE f.id = :formId)")
     boolean existsByProduct_FormId(@Param("formId")Long formId);
+
+    @Query("SELECT o FROM Order o WHERE o.deletedAt IS NULL AND o.buyer = :loginMember")
+    List<Order> findByBuyer(@Param("loginMember") Member loginMember);
+
+    @Query("SELECT o FROM Order o WHERE o.deletedAt IS NULL AND o.form = :form")
+    List<Order> findAllByForm(@Param("form") Form form);
 }
