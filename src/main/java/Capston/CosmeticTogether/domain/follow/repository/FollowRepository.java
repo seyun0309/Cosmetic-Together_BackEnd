a2d3b74e888109e724e8fbfb2015e9e1275662ab
seyun kim <>
2025-05-27 11:24:45 +0900

    FEAT : 로그인 사용자가 게시글 작성자 팔로우 했는지에 대한 유무 메서드

--- src/main/java/Capston/CosmeticTogether/domain/follow/repository/FollowRepository.java
+++ src/main/java/Capston/CosmeticTogether/domain/follow/repository/FollowRepository.java
@@ -1,7 +1,11 @@
 package Capston.CosmeticTogether.domain.follow.repository;
 
 import Capston.CosmeticTogether.domain.follow.domain.Follow;
+import Capston.CosmeticTogether.domain.member.domain.Member;
+import io.swagger.v3.oas.annotations.Parameters;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +13,7 @@ import java.util.Optional;
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     Follow findByFollowerIdAndFollowingId(Long followingId, Long id);
+
+    @Query("SELECT f FROM Follow f WHERE f.follower = :loginMember AND f.following = :writer AND f.isValid = true")
+    Optional<Follow> findByFollowerAndFollowingAndIsValidTrue(@Param("loginMember") Member loginMember, @Param("writer") Member writer);
 }
