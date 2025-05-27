deeac9e0337aa122c7b489f60bb8173d0ed5948a
seyun kim <>
2025-05-27 11:29:19 +0900

    FEAT : 주소 변경 요쳥 DTO 추가

---
+++ src/main/java/Capston/CosmeticTogether/domain/member/dto/request/AddressUpdateRequestDTO.java
@@ -0,0 +1,13 @@
+package Capston.CosmeticTogether.domain.member.dto.request;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AddressUpdateRequestDTO {
+    private String address;
+}
