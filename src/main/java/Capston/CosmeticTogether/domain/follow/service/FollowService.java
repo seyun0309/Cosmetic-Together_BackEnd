97571fb19acc025186aea0d5b19e30342048f7b6
seyun kim <>
2025-05-28 22:23:51 +0900

    FEAT : 게시글, 폼 나눠서 팔로우/언팔로우 서비스 로직 추가

--- src/main/java/Capston/CosmeticTogether/domain/follow/service/FollowService.java
+++ src/main/java/Capston/CosmeticTogether/domain/follow/service/FollowService.java
@@ -7,6 +7,8 @@ import Capston.CosmeticTogether.domain.follow.domain.Follow;
 import Capston.CosmeticTogether.domain.follow.dto.response.FollowResponseDTO;
 import Capston.CosmeticTogether.domain.follow.dto.response.GetFollowAndFollowingMemberDTO;
 import Capston.CosmeticTogether.domain.follow.repository.FollowRepository;
+import Capston.CosmeticTogether.domain.form.domain.Form;
+import Capston.CosmeticTogether.domain.form.repository.FormRepository;
 import Capston.CosmeticTogether.domain.member.domain.Member;
 import Capston.CosmeticTogether.domain.member.repository.MemberRepository;
 import Capston.CosmeticTogether.domain.member.service.MemberService;
@@ -32,9 +34,10 @@ public class FollowService {
     private final MemberService memberService;
     private final AuthUtil authUtil;
     private final BoardRepository boardRepository;
+    private final FormRepository formRepository;
 
     @Transactional
-    public FollowResponseDTO likeOrUnlikeBoard(Long boardId) {
+    public FollowResponseDTO likeOrUnlikeBoardByBoardId(Long boardId) {
 
         Member loginMember = authUtil.extractMemberAfterTokenValidation();
 
@@ -72,6 +75,47 @@ public class FollowService {
             return new FollowResponseDTO(true, followingMember.getNickname());
         }
     }
+
+    @Transactional
+    public FollowResponseDTO likeOrUnlikeBoardByFormId(Long formId) {
+
+        Member loginMember = authUtil.extractMemberAfterTokenValidation();
+
+        Form form = formRepository.findById(formId).orElseThrow(() -> new BusinessException(ErrorCode.BOARD_NOT_FOUND));
+
+        Long followingId = form.getOrganizer().getId();
+
+        // 자신을 팔로우하려는 경우 예외 처리
+        if (loginMember.getId().equals(followingId)) {
+            throw new BusinessException("자기 자신을 팔로우할 수 없습니다", ErrorCode.SELF_FOLLOW);
+        }
+
+        // DB에 있는 id인지 검사
+        Member followingMember = memberRepository.findById(followingId).orElseThrow(() -> new BusinessException("해당 사용자가 존재하지 않습니다", ErrorCode.MEMBER_NOT_FOUND));
+
+        // 팔로우 여부 확인
+        Follow checkFollow = followRepository.findByFollowerIdAndFollowingId(loginMember.getId(), followingId);
+
+        if (checkFollow != null) {
+            if(checkFollow.isValid()) {
+                checkFollow.setValid(false);
+                return new FollowResponseDTO(false, followingMember.getNickname());
+            } else {
+                checkFollow.setValid(true);
+                return new FollowResponseDTO(false, followingMember.getNickname());
+            }
+        } else {
+            // 팔로우 처리
+            Follow follow = Follow.builder()
+                    .follower(loginMember)
+                    .following(followingMember)
+                    .isValid(true)
+                    .build();
+            followRepository.save(follow);
+            return new FollowResponseDTO(true, followingMember.getNickname());
+        }
+    }
+
 
     public List<GetFollowAndFollowingMemberDTO> getFollowers() {
         // 1. 로그인 한 사용자 가져오기
