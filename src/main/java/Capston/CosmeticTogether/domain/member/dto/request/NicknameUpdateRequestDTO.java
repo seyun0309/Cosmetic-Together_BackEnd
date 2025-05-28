9616ec8e99665c44ec1311dec74bfdd5b1ba061c
seyun kim <>
2025-05-28 22:26:28 +0900

    FEAT : 닉네임 수정시 요청 DTO 추가

---
+++ src/main/java/Capston/CosmeticTogether/domain/member/dto/request/NicknameUpdateRequestDTO.java
@@ -0,0 +1,13 @@
+package Capston.CosmeticTogether.domain.member.dto.request;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class NicknameUpdateRequestDTO {
+    private String nickName;
+}
