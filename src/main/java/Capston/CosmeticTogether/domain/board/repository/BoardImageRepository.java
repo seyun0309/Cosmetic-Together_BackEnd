ffa8f4d91d41691bc1e5419ec1c08299a4679331
seyun kim <>
2025-06-05 19:11:32 +0900

    FEAT : 게시글 이미지 삭제 메서드 추가

--- src/main/java/Capston/CosmeticTogether/domain/board/repository/BoardImageRepository.java
+++ src/main/java/Capston/CosmeticTogether/domain/board/repository/BoardImageRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardImageRepository extends JpaRepository<BoardImage, Long> {
+
+
+    void deleteByBoardUrl(String boardUrl);
 }
